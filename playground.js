0b04ddd66b8420ed4998413ce2914c01302dc55c
pagorn <pagorn.phusaisakul@oivan.com>
2021-07-22 23:51:55 +0700

    Update playground.js

--- playground.js
+++
@@ -1,8 +0,0 @@
-const { generateAccessToken } = require('./services/auths');
-const usersService = require('./services/users')
-const passwordsService = require('./services/passwords')
-
-const user = usersService.findByCredential('nrogapp', 'Samp1e-password')
-
-console.log(generateAccessToken(user, 'user'))
-// console.log(passwordsService.hash('Samp1e-password'))
