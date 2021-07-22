46756fd91e955e5e22ecad441dfec3d76a152396
pagorn <pagorn.phusaisakul@oivan.com>
2021-07-22 23:43:16 +0700

    Update /users/signin

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,21 @@
 const express = require('express')
 
+const { findByCredential } = require('../services/users')
+const { generateAccessToken } = require('../services/auths')
+
 const router = express.Router()
 
-router.get('/signin', function (req, res) {
-  res.send('ok')
+router.post('/signin', async function (req, res) {
+  const { username, password } = req.body
+  const user = await findByCredential(username, password)
+
+  if (user === null) {
+    return res.sendStatus(403)
+  }
+
+  user.token = generateAccessToken(user)
+
+  res.json(user)
 })
 
 module.exports = router
